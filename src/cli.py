60ed43862e4ee008d048811ecf9ee7008c4a98d2
Aidan Griffiths <aidanjames.g@gmail.com>
2021-01-19 02:10:07 +1100

    feat: improved private user enrollment and functional mfcc conversion to user specific dir

--- src/cli.py
+++ src/cli.py
@@ -1,38 +1,54 @@
 import argparse
+from subprocess import Popen
 
 parser = argparse.ArgumentParser(
          description="Process new user enrollment for speaker verification validation"
      )
-     parser.add_argument(
-         "--user-id",
+parser.add_argument(
+         "--username",
          type=str,
          required=True,
-         help="User identification number for new user.",
+         help="User identifying name for a new user.",
      )
-     parser.add_argument(
-         "--audiofile-path",
+parser.add_argument(
+          "--password",
+          type=str,
+          required=True,
+          help="User identifying password for new user.",
+      )
+parser.add_argument(
+         "--audio-path",
          type=str,
          required=True,
          help="Path to enrollment audio, required for baseline verification.",
      )
 
-     parser.add_argument(
+parser.add_argument(
           "--port",
           type=int,
           default=5000,
           help="local port number to flask instance (default = 5000).",
       )
 
-     args = parser.parse_args()
+args = parser.parse_args()
 
 def process_new_user():
-    args.user_id
-    Popen(f"curl -X POST localhost:{port}/users -d \"user_id={user_id}\""
-    Popen(f"curl -X POST localhost:{port}/users -d \"audio_file={audiofile_path}\""
-    Popen(f"curl -X POST -F file=@./test.wav http://localhost:5000/file-upload"
-    args.audiofile_path
-    return 0
+    try:
+        enroll_user = Popen(["curl", "-i", "-X", "POST", "-H", "Content-Type: application/json", "-d", "\'{\"username\":\"{args.username}\",\"password\":\"{args.password}\"}\'", f"localhost:{args.port}/api/users"])
+    except Exception:
+        print("BAD USER ENROLLMENT")
+    try:
+        upload_file = Popen(["curl", "-u", f"{args.username}:{args.password}", "-X", "POST", "-F",  f"file=@{args.audio_path} http://localhost:5000/api/uploads"])
+    except Exception:
+        print("BAD UPLOAD OF FILE!")
 
-def put_user_en
+if __name__ == "__main__":
+    process_new_user()
 
 
+    """
+
+    curl -i -X POST -H "Content-Type: application/json" -d '{"username":"jacob","password":"python"}' http://127.0.0.1:5000/api/users
+
+    curl -u aidan:python -X POST -F file=@./p225_012_mic1.flac http://localhost:5000/api/uploads
+    """
