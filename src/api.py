0fed1312a798858ee35b0614bbf50e5400b6ab39
Aidan Griffiths <aidanjames.g@gmail.com>
2021-01-19 02:10:07 +1100

    feat: improved private user enrollment and functional mfcc conversion to user specific dir

--- src/api.py
+++ src/api.py
@@ -1,151 +1,157 @@
 import os
-import urllib.request
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
+import time
+import pathlib
+import pickle
+
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+import jwt
 from werkzeug.utils import secure_filename
-from flask_restful import reqparse, abort, Api, Resource
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from audio import NUM_FRAMES, SAMPLE_RATE, read_mfcc, sample_from_mfcc
 
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'mp3', 'flac', 'wav'}
 
+# initialization
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-api = Api(app)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-USERS = {
-    'id1' : {
-                'audio_file': "mfcc obj",
-                'mfcc': "path/to/mfcc"
-            }
-}
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+class InputAudioError(Exception):
+    pass
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expires_in=600):
+        return jwt.encode(
+            {'id': self.id, 'exp': time.time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+                              algorithms=['HS256'])
+        except:
+            return
+        return User.query.get(data['id'])
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/file-upload', methods=['POST'])
+
+@app.route('/api/uploads', methods=['POST'])
+@auth.login_required
 def upload_file():
-	# check if the post request has the file parti
-	if 'file' not in request.files:
-		resp = jsonify({'message' : 'No file part in the request'})
-		resp.status_code = 400
-		return resp
-	file = request.files['file']
-	if file.filename == '':
-		resp = jsonify({'message' : 'No file selected for uploading'})
-		resp.status_code = 400
-		return resp
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		resp = jsonify({'message' : f'{filename} File successfully uploaded'})
-		resp.status_code = 201
-		return resp
-	else:
-		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
-		resp.status_code = 400
-		return resp
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-def check_if_user_exists(user_id):
-    if user_id not in USERS:
-        abort(404, message="User {} doesn't exist".format(user_id))
-
-
-@app.route('/file-upload', methods=['POST'])
-def upload_file(self, user_id):
     # check if the post request has the file part
+    id = g.user.username
     if 'file' not in request.files:
         resp = jsonify({'message' : 'No file part in the request'})
         resp.status_code = 400
         return resp
-    file = request.files['file']
-    if file.filename == '':
+    file_ = request.files['file']
+    if file_.filename == '':
         resp = jsonify({'message' : 'No file selected for uploading'})
         resp.status_code = 400
         return resp
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{user_id}-{filename}"))
-        resp = jsonify({'message' : f'{filename} File successfully uploaded'})
-        resp.status_code = 201
+    if file_ and allowed_file(file_.filename):
+        filename = secure_filename(file_.filename)
+        output_path = pathlib.Path(f"./api/uploads/{id}")
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        resp = convert_input_to_mfcc(file_, output_path)
+        #file.save(pathlib.Path(f"./api/uploads/{id}", f"{filename}"))
         return resp
     else:
-        resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp = jsonify({'message' : 'Allowed file types are mp3, wav and flac'})
         resp.status_code = 400
         return resp
 
-parser = reqparse.RequestParser()
-parser.add_argument('user_id')
-parser.add_argument('audio_file')
-parser.add_argument('mfcc')
+@auth.login_required
+def convert_input_to_mfcc(audio, output_path):
+    try:
+        if audio is not None:
+            mfcc = sample_from_mfcc(read_mfcc(audio, SAMPLE_RATE), NUM_FRAMES)
+            with open(pathlib.Path(output_path, f"{audio.filename}.pkl"), 'wb') as pkl:
+                pickle.dump(mfcc, pkl)
 
-class User(Resource):
-    def get(self, user_id):
-        check_if_user_exists(user_id)
-        return USERS[user_id]
+            return jsonify({'message' : f'{audio.filename} successfully uploaded for {output_path}'}), 201
 
-    def delete(self, user_id):
-        check_if_user_exists(user_id)
-        del USERS[user_id]
-        return '', 204
-
-    def put(self, user_id):
-        args = parser.parse_args()
-        task = {
-                'audio_file': args['audio_file'],
-                'mfcc': "path/to/pickle_file"
-               }
-        USERS[user_id] = task
-        return task, 201
-
-#    @app.route('/file-upload', methods=['POST'])
-#    def upload_file(self, user_id):
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#                 resp = jsonify({'message' : 'No file part in the request'})
-##                 resp.status_code = 400
-#                 return resp
-#         file = request.files['file']
-#         if file.filename == '':
-#                 resp = jsonify({'message' : 'No file selected for uploading'})
-#                 resp.status_code = 400
-#                 return resp
-#         if file and allowed_file(file.filename):
-#                 filename = secure_filename(file.filename)
-#                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{user_id}-{filename}"))
-#                 resp = jsonify({'message' : f'{filename} File successfully uploaded'})
-#                 resp.status_code = 201
-#                 return resp
-#         else:
-#                 resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
-#                 resp.status_code = 400
-#                 return resp
+    except InputAudioError:
+        return jsonify({'message' : f'Allowed file types are {ALLOWED_EXTENSIONS}'}), 400
 
 
-class UserList(Resource):
-    def get(self):
-        return USERS
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
-    def post(self):
-        args = parser.parse_args()
-        # revist later as this info should not be depending of user input
-        #user_id = int(max(USERS.keys()).lstrip('id')) + 1
-        #user_id = 'id%i' % user_id
-        user_id = args['user_id']
-        USERS[user_id] = {
-            'audio_file': args['audio_file'],
-            'mfcc': "path/to/pickle_file"
-            }
-        return USERS[user_id], 201
 
-"""Setup the Api resource routing here"""
-api.add_resource(UserList, '/users')
-api.add_resource(User, '/users/<user_id>')
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
 
 if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
     app.run(debug=True)
