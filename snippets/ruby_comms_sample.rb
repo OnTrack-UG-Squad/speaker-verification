39cf7aafd008f69a3d1a2f5ea8f52248f871c126
Aidan Griffiths <aidanjames.g@gmail.com>
2021-05-16 13:59:02 +1000

    docs: adding comments to sample scripts use to external ruby projects

--- snippets/ruby_comms_sample.rb
+++ snippets/ruby_comms_sample.rb
@@ -1,7 +1,11 @@
 require 'open3'
+# sample script to demonstrate ruby functionality for using open3 to trigger 
+# speaker verification over the command line.
+# output is given to system stdout call which is received by external ruby project.
+# Requires external ruby environment to trigger.
 
 id = 111111112
-filePath = "./speaker_verification/tests/input"
+filePath = "../speaker_verification/tests/input"
 
 enrollment = "python -m speaker_verification enroll --id #{id} --audio-path #{filePath}/enrollment.flac"
 stdout, stderr, status = Open3.capture3(enrollment)
