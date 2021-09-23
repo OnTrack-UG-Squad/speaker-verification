15d4864495086e66c5ac36773469842af295f91a
Josh Hurley <jhurley17@gmail.com>
2021-09-23 16:37:21 +1000

    feat: Add setup.py
    Co-authored-by: Victoria-Mag <Victoria-Mag@users.noreply.github.com>
    Co-authored-by: KuzzyKabana <KuzzyKabana@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="speaker-verification",
+    version="0.1.3",
+    author="OnTrack UG Squad",
+    author_email="hurleyjos@deakin.edu.au",
+    description="A small example package",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/OnTrack-UG-Squad/speaker-verification",
+    project_urls={
+        "Bug Tracker": "https://github.com/OnTrack-UG-Squad/speaker-verification/issues",
+        "Pull Requests": "https://github.com/OnTrack-UG-Squad/speaker-verification/pulls"
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities"
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires = [">=3.8", "<3.9"],
+    zip_safe = True,
+    include_package_data = True,
+    install_requires = [
+        "audioread ==2.1.9",
+        "Keras ==2.3.1",
+        "librosa ==0.7.2",
+        "numba ==0.48.0",
+        "numpy ==1.18.5",
+        "python-speech-features ==0.6",
+        "tensorflow >=2.1.0",
+        "pytest"
+    ]
+)
